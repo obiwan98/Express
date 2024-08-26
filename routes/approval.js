09033a0c59d899f8045098e2b127bee011f02d72
이장열 <yeol2011@cj.net>
2024-08-26 15:08:33 +0900

    /api/approvals/list API 추가

--- routes/approval.js
+++ routes/approval.js
@@ -1,40 +1,58 @@
-const express = require("express");
-// const { Approval, Book, Payment, Confirm } = require("../models/approval");
-const Approval = require("../models/approval");
-const User = require("../models/user");
-const Group = require("../models/group");
-const auth = require("../middlewares/auth");
-const axios = require("axios");
+const express = require('express');
+const Approval = require('../models/approval');
+const User = require('../models/user');
+const Group = require('../models/group');
+const auth = require('../middlewares/auth');
+const axios = require('axios');
 const router = express.Router();
 
-router.post("/test", async (req, res) => {
-  // const { email, name, password, role, group } = req.body;
-  const user = await User.findOne({ email: "yeol2011@cj.net" }).populate(
-    "group"
-  );
-  console.log(user);
+router.get('/list/:state', auth, async (req, res) => {
   try {
+    const approvals = await Approval.find({
+      state: req.params.state,
+      group: req.group._id,
+    })
+      .populate('user', 'email name password signupDate')
+      .populate('group')
+      .populate('confirm.user', 'email name password signupDate')
+      .populate('confirm.group')
+      .populate('payment.user', 'email name password signupDate')
+      .populate('payment.group');
+
+    res.status(200).send(approvals);
+  } catch (error) {
+    console.error('Error fetching approval list:', error);
+    res.status(400).send({ errorCode: error.code, error: error.errmsg });
+  }
+});
+
+router.post('/test', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: 'yeol2011@cj.net' }).populate(
+      'group'
+    );
+
     const approval = new Approval({
       user: user._id,
       group: user.group._id,
       book: {
-        name: "리액트마스터1",
+        name: '리액트마스터1',
         price: 15000,
       },
-      comment: "도서요청해요1",
+      comment: '도서요청해요1',
       regdate: Date.now(),
-      state: "0",
+      state: '2',
       confirm: {
         user: user._id,
         group: user.group._id,
         date: Date.now(),
-        comment: "승인합니다.",
+        comment: '승인합니다.',
       },
       payment: {
         user: user._id,
         group: user.group._id,
-        receiptInfo: "영수증정보입니다.",
-        receiptImgUrl: "aa",
+        receiptInfo: '영수증정보입니다.',
+        receiptImgUrl: 'aa',
         price: 15000,
         date: Date.now(),
       },
@@ -42,9 +60,9 @@ router.post("/test", async (req, res) => {
 
     // 사용자 저장
     await approval.save();
-    res.status(201).send({ message: "데이터가 입력되었습니다." });
+    res.status(201).send({ message: '데이터가 입력되었습니다.' });
   } catch (error) {
-    console.error("Error during signup:", error);
+    console.error('Error during signup:', error);
     res.status(400).send({ errorCode: error.code, error: error.errmsg });
   }
 });
