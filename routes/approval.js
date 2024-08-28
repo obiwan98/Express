31df277c84847cf50de2c5b8a6c7a5c3189dd15e
EINE_CSB <seulbeom.choi@cj.net>
2024-08-28 15:31:29 +0900

    240828 중간 리뷰 작업 저장(To. Express_approval)

--- routes/approval.js
+++ routes/approval.js
@@ -70,6 +70,68 @@ router.post('/api/approvals/test', async (req, res) => {
 });
 
 //승인관리(최슬범님)
+// 승인 전체 조회 TEST
+router.get('/api/approvals', async (req, res) => {
+  try {
+    const approvals = await Approval.find();
+    console.log(approvals);
+    res.status(200).send(approvals);
+  } catch (error) {
+    console.error('Error fetching approvals:', error);
+    res.status(500).send({ error: 'Internal server error' });
+  }
+});
+
+// 승인 요청(신규) TEST
+router.post('/api/approvals/pending', async (req, res) => {
+  try {
+    const { reqItems, etc } = req.body.data;
+
+    const getValueByKey = (items, key) => {
+      const item = items.find((item) => item.key === key);
+      return item ? item.value : null;
+    };
+
+    const itemComment = getValueByKey(reqItems, 'comment');
+    const itemState = getValueByKey(reqItems, 'state');
+    const userEmail = etc.email;
+
+    // Email로 user정보 갖고오기
+    const user = await User.findOne({ email: userEmail }).populate('group');
+
+    const approval = new Approval({
+      user: user._id,
+      group: user.group._id,
+      book: {
+        name: null,
+        price: null,
+      },
+      comment: itemComment,
+      regdate: Date.now(),
+      state: itemState,
+      confirm: {
+        user: user._id,
+        group: user.group._id,
+        date: Date.now(),
+        comment: 'Approved',
+      },
+      payment: {
+        user: user._id,
+        group: user.group._id,
+        receiptInfo: null,
+        receiptImgUrl: null,
+        price: null,
+        date: Date.now(),
+      },
+    });
+
+    await approval.save();
+    res.status(201).send({ message: '데이터가 입력되었습니다.' });
+  } catch (error) {
+    console.error('Error during pending:', error);
+    res.status(400).send({ errorCode: error.code, error: error.errmsg });
+  }
+});
 
 //승인관리(신혜경님)
 
