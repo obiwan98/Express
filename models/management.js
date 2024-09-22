4921fc61e74ec46a10d77f1a9b45cb87303b5a0c
HaeJin-L <haejin.lee2@cj.net>
2024-09-22 09:22:05 +0900

    도서 관리 > 도서 조회 컴포넌트 구조 변경

--- models/management.js
+++ models/management.js
@@ -2,15 +2,15 @@
 const mongoose = require('mongoose');
 
 const managementSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: false },
-    cover : { type: String, required: false },
-    publisher: { type: String, required: false },
-    publicationDate: { type: Date, default: Date.now },
-    count: { type: Number, default: 1 },
-    registrationDate:  { type: Date, default: Date.now },
-    group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
-    registeredBy: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: false },
+  cover: { type: String, required: false },
+  publisher: { type: String, required: false },
+  publicationDate: { type: Date, default: Date.now },
+  count: { type: Number, default: 1 },
+  registrationDate: { type: Date, default: Date.now },
+  group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
+  registeredBy: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Management', managementSchema, "bookList");
+module.exports = mongoose.model('Management', managementSchema, 'bookList');
--- routes/management.js
+++ routes/management.js
@@ -1,14 +1,26 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const Management = require("./../models/management");
+const Management = require('./../models/management');
 
-const upload = require("./../middlewares/upload");
+const upload = require('./../middlewares/upload');
 
 // 도서 등록
-router.post("/api/management/bookAdd", upload.single("cover"), async (req, res) => {
-    const { title, author, publisher, publicationDate, count, registrationDate, group, registeredBy } = req.body;
-    const cover = req.file?.filename || "";
+router.post(
+  '/api/management/bookAdd',
+  upload.single('cover'),
+  async (req, res) => {
+    const {
+      title,
+      author,
+      publisher,
+      publicationDate,
+      count,
+      registrationDate,
+      group,
+      registeredBy,
+    } = req.body;
+    const cover = req.file?.filename || '';
 
     try {
       const bookData = new Management({
@@ -20,49 +32,82 @@ router.post("/api/management/bookAdd", upload.single("cover"), async (req, res) 
         count,
         registrationDate,
         group,
-        registeredBy
+        registeredBy,
       });
-  
+
       await bookData.save();
-  
-      res.status(200).send({ message: "도서를 등록하였습니다." });
+
+      res.status(200).send({ message: '도서를 등록하였습니다.' });
     } catch (error) {
-      res.status(400).send({ errorCode: error.code, error: error.errmsg });
+      res.status(500).send({ errorCode: error.code, error: error.errmsg });
     }
-});
+  }
+);
 
 // 도서 조회
-router.get("/api/management/bookList", async (req, res) => {
+router.get('/api/management/bookList', async (req, res) => {
+  try {
+    const { title, group } = req.query;
+
+    const escapeRegExp = (string) =>
+      string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+    const query = {
+      ...(title && { title: new RegExp(escapeRegExp(title), 'i') }),
+      ...(group && { group }),
+    };
+
+    const bookList = await Management.find(query);
+
+    res.status(200).json(bookList);
+  } catch (error) {
+    res.status(500).send({ errorCode: error.code, error: error.errmsg });
+  }
+});
+
+// 도서 변경
+router.put(
+  '/api/management/bookUpdate/:id',
+  upload.single('cover'),
+  async (req, res) => {
+    const { title, author, publisher, publicationDate, count } = req.body;
+    const cover = req.file?.filename || '';
+
     try {
-      const { title, group } = req.query;
-  
-      const escapeRegExp = (string) =>
-        string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-  
-      const query = {
-        ...(title && { title: new RegExp(escapeRegExp(title), "i") }),
-        ...(group && { group }),
-      };
-  
-      const bookList = await Management.find(query);
-  
-      res.status(200).json(bookList);
+      const bookData = await Management.findByIdAndUpdate(
+        req.params.id,
+        {
+          title,
+          author,
+          ...(cover && { cover }),
+          publisher,
+          publicationDate,
+          count,
+        },
+        { new: true, runValidators: true }
+      );
+
+      if (!bookData)
+        res.status(404).send({ error: '도서를 찾을 수 없습니다.' });
+
+      res.status(200).send({ message: '도서를 변경하였습니다.' });
     } catch (error) {
-    res.status(400).send({ errorCode: error.code, error: error.errmsg });
+      res.status(500).send({ errorCode: error.code, error: error.errmsg });
     }
-  });
+  }
+);
 
 // 도서 삭제
-router.delete("/api/management/bookDelete/:id", async (req, res) => {
-    try {
-      const bookData = await Management.findByIdAndDelete(req.params.id);
-  
-      if (!bookData) res.status(404).send({ error: "도서를 찾을 수 없습니다." });
-  
-        res.status(200).send({ message: "도서를 삭제하였습니다." });
-    } catch (error) {
-        res.status(400).send({ errorCode: error.code, error: error.errmsg });
-    }
+router.delete('/api/management/bookDelete/:id', async (req, res) => {
+  try {
+    const bookData = await Management.findByIdAndDelete(req.params.id);
+
+    if (!bookData) res.status(404).send({ error: '도서를 찾을 수 없습니다.' });
+
+    res.status(200).send({ message: '도서를 삭제하였습니다.' });
+  } catch (error) {
+    res.status(500).send({ errorCode: error.code, error: error.errmsg });
+  }
 });
 
 module.exports = router;
