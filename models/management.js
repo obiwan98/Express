aaa5370ae700d17a0d71912aa3837f0d7190338d
haejin <haejin891212@gmail.com>
2024-09-26 15:11:47 +0900

    1. 도서 대여 기능 추가

--- models/management.js
+++ models/management.js
@@ -1,13 +1,20 @@
 // models/management.js
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
+const reviewsSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
-  rate: { type: Number, default: 0 },
+  rate: { type: Number, default: 0, min: 0, max: 5 },
   tag: { type: Number, required: true },
   comment: { type: String, required: true },
   registrationDate: { type: Date, default: Date.now },
+  registeredBy: { type: String, required: true },
+});
+
+const historySchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
   registeredBy: { type: String, required: true },
 });
 
@@ -18,7 +25,8 @@ const managementSchema = new mongoose.Schema({
   publisher: { type: String, required: false },
   publicationDate: { type: Date, default: Date.now },
   count: { type: Number, default: 1 },
-  review: [reviewSchema],
+  reviews: [reviewsSchema],
+  history: [historySchema],
   registrationDate: { type: Date, default: Date.now },
   group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
   registeredBy: { type: String, required: true },
